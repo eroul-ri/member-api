a86fd1efc06de10479bc35fa7a18795df20e1f1a
eroul-ri <bhr139313@gmail.com>
2023-01-14 04:17:41 +0900

    패스워드 인코딩 test ok

--- src/main/java/com/eroul/api/config/SecurityConfig.java
+++ src/main/java/com/eroul/api/config/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -31,5 +33,10 @@ public class SecurityConfig {
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
--- src/test/java/com/eroul/api/ApiApplicationTests.java
+++ src/test/java/com/eroul/api/ApiApplicationTests.java
@@ -1,13 +1,36 @@
 package com.eroul.api;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@Slf4j
 @SpringBootTest
+@RunWith(SpringRunner.class)
 class ApiApplicationTests {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Test
     void contextLoads() {
+        Assert.assertNotNull(passwordEncoder);
     }
 
+    /**
+     * Bcrypt Encode 테스트
+     */
+    @Test
+    public void passwordEncodingTest() {
+        String password = "#1234";
+
+        String encPassword = passwordEncoder.encode(password);
+
+        Assert.assertTrue(passwordEncoder.matches(password, encPassword));
+    }
 }
