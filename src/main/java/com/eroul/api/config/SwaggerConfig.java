3b630dda15b3e55d812f41e59de3e61aeb904fe6
eroul-ri <bhr139313@gmail.com>
2023-01-15 06:20:36 +0900

    spring doc으로 변경

--- src/main/java/com/eroul/api/config/SwaggerConfig.java
+++ src/main/java/com/eroul/api/config/SwaggerConfig.java
@@ -1,33 +1,14 @@
 package com.eroul.api.config;
 
-import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
+@OpenAPIDefinition(
+        info = @Info(title = "Member API Documentation",
+                description = "회원 정보 관리 API Documentation",
+                version = "v1"))
 @Configuration
 public class SwaggerConfig {
 
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.OAS_30)
-                .useDefaultResponseMessages(true)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.eroul.api.member.controller"))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo());
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("MEMBER API Documentation")
-                .description("회원 API 문서 ver1.0")
-                .version("1.0")
-                .build();
-    }
 }
