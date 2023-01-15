2a3bd56347e1638b665474deac0c9f659a00602c
eroul-ri <bhr139313@gmail.com>
2023-01-15 18:34:30 +0900

    Swagger Request Schema 작성

--- src/main/java/com/eroul/api/member/dto/MemberSignInReq.java
+++ src/main/java/com/eroul/api/member/dto/MemberSignInReq.java
@@ -1,5 +1,6 @@
 package com.eroul.api.member.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -8,8 +9,11 @@ import javax.validation.constraints.NotBlank;
 @Getter
 @AllArgsConstructor
 public class MemberSignInReq {
+
     @NotBlank
+    @Schema(description = "이메일주소 또는 휴대전화번호")
     private String username;
     @NotBlank
+    @Schema(description = "패스워드")
     private String password;
 }
--- src/main/java/com/eroul/api/member/dto/VeriPhReq.java
+++ src/main/java/com/eroul/api/member/dto/VeriPhReq.java
@@ -1,19 +1,23 @@
 package com.eroul.api.member.dto;
 
-import com.eroul.api.common.validator.Password;
 import com.eroul.api.common.validator.PhNumber;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 @Getter
 @AllArgsConstructor
-public class RePasswordReq {
-    @Password
-    @Schema(description = "변경할 패스워드", maxLength = 30)
-    private String rePassword;
+public class VeriPhReq {
 
     @PhNumber
     @Schema(description = "휴대전화번호", maxLength = 11)
     private String phNumber;
+
+    @NotBlank
+    @Pattern(regexp = "^[0-9]{4,}$", message = "인증번호는 숫자 4자리로 입력해주세요.")
+    @Schema(description = "인증 번호", maxLength = 4)
+    private String veriNumber;
 }
