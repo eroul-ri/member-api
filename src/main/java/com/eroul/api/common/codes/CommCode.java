ef7a4f2df030c85e35fdb29c98c65bff3972e555
eroul-ri <bhr139313@gmail.com>
2023-01-15 20:30:47 +0900

    코드 정렬

--- src/main/java/com/eroul/api/common/codes/CommCode.java
+++ src/main/java/com/eroul/api/common/codes/CommCode.java
@@ -8,11 +8,11 @@ import lombok.Getter;
 public enum CommCode {
     SUCCESS("0000", "Request Success")
     , FAIL("0001", "Request Fail")
-    , ERROR("-9998", "Error")
-    , NOTFOUND("-4444", "Not Found")
-    , BADREQ("-9999", "Bad Request")
     , UNAUTHORIZED("-4111", "Unauthorized Request")
-    , FORBIDDEN("-4333", "FORBIDDEN");
+    , FORBIDDEN("-4333", "FORBIDDEN")
+    , NOTFOUND("-4444", "Not Found")
+    , ERROR("-9998", "Error")
+    , BADREQ("-9999", "Bad Request");
 
     private String code;
     private String message;
